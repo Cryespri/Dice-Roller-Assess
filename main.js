68a82733ed50cc6d0953abf1f9d285367960c6b4
Crystal Douglas <cryespri@gmail.com>
2020-05-21 20:44:06 -0500

    Added some code, but stuck on where to go next

---
+++ main.js
@@ -0,0 +1,39 @@
+//Do step 1 first dieRolls variable should be outside.
+
+const rollButton = document.querySelector('.roll-button')
+const showRollsButton = document.querySelector('.show-all-rolls')
+const dieRollsArray = []//empty array
+let total = document.querySelector('#total-rolls')
+let userInput = document.querySelector('#number-of-dice')
+let newTotal = 0
+
+
+rollButton.addEeventListener("click", function () {
+
+    // 1. Get the value of the input text box and assign it
+
+    // 2. Loop THAT number of times 
+
+    let counter = 0
+    while (counter < userInput.value) {
+
+        let diceRoll = Math.ceil(Math.random() * 6);
+        console.log(diceRoll);
+        counter++
+        dieRollsArray.push(diceRoll)
+        newtotal += diceRoll
+
+    }
+    total.innerHTML = newTotal
+
+})
+// 3. nd each time "roll" randomly from 1 to 6
+// Loop a numner of times equal to the number-of-dice.
+
+
+showRollsButton.addEventListener("click", function () {
+    let counter = 0
+    while (counter < dieRollsArray.length) {
+        counter +=
+   }
+}
