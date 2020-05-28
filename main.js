63761a44c106f4419e4160bfdaaee91192428c0c
Crystal Douglas <cryespri@gmail.com>
2020-05-27 19:10:24 -0500

    added the Show All Rolls Button, shows all dice rolled

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 //Do step 1 first dieRolls variable should be outside.
 
 const rollButton = document.querySelector('.roll-button')
-const showRollsButton = document.querySelector('.show-all-rolls')
+const showRollsButton = document.querySelector('#show-all-rolls')
 const dieRollsArray = []//empty array
 let total = document.querySelector('#total-rolls')
 let userInput = document.querySelector('#number-of-dice')
 let newTotal = 0
-
+const allRollsButton = document.querySelector('#all-rolls')
 
 rollButton.addEventListener("click", function () {
 
@@ -27,25 +27,12 @@ rollButton.addEventListener("click", function () {
     total.innerHTML = newTotal
 
 })
-// 3. nd each time "roll" randomly from 1 to 6
-// Loop a numner of times equal to the number-of-dice.
-
+// Shows all numbers that were rolled
 
 showRollsButton.addEventListener("click", function () {
     let counter = 0
     while (counter < dieRollsArray.length) {
-        counter += 1
+        allRollsButton.innerHTML += "<li>" + dieRollsArray[counter] + "</li>";
+        counter++
     }
 })
-
-/* function sumDiceRolls() {
-    let total = 0
-    let index = 0
-    while (index < diceRolls.length) {
-        total = total + diceRolls[index]
-
-        index++
-    }
-
-    return total
-*/
